fba3d4711736630560c18ea451511d594bb8e5dd
JoniKarta <jkarta26@gmail.com>
2020-03-25 18:00:27 +0200

    change

--- 2020b.jonathan.karta/src/main/java/acs/Application.java
+++ 2020b.jonathan.karta/src/main/java/acs/Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 	// SpringBootBoot
+	// print
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
