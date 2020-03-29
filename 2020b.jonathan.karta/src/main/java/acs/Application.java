3f3008c19a5f72b341d0edaa307718dc260bd321
Lapis lazuli <djfarag@gmail.com>
2020-03-30 00:20:21 +0300

    bored people

--- 2020b.jonathan.karta/src/main/java/acs/Application.java
+++ 2020b.jonathan.karta/src/main/java/acs/Application.java
@@ -6,12 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 
-	// Jonathan karta the king
-
-	// Jonathan karta
-
-	// SprintBootOriginal
-
+	// SprintBoot
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
