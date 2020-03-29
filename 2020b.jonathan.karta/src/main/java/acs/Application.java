660e2b49954674367c43e72f453c3a805929ac04
Lapis lazuli <djfarag@gmail.com>
2020-03-30 00:04:24 +0300

    conflictFix

--- 2020b.jonathan.karta/src/main/java/acs/Application.java
+++ 2020b.jonathan.karta/src/main/java/acs/Application.java
@@ -5,13 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
-<<<<<<< HEAD
-	// SprintBoot1
-||||||| merged common ancestors
-	// SprintBoot
-=======
-	// Jonathan karta
->>>>>>> 487cc3e5ce2cfca288597c809a5c3d5a3e76ce46
+	// SprintBootOriginal
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
